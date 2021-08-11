974b7c8473d73ba3f6a4c78c2721c74d5e865960
Deepak Deedar Singla <ddeedar.cse18@chitkarauniversity.edu.in>
2021-08-11 13:39:05 +0530

    initial commit

---
+++ iamrich/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: Text('I am Rich'),
+          backgroundColor: Colors.blueGrey[900],
+          centerTitle: true,
+        ),
+        body: Center(
+          child: Image(
+            image: AssetImage('images/diamond.gif'),
+          ),
+        ),
+      ),
+    ),
+  );
+}
