219d242167daeca6dfd73a18597552f1529757d0
Deepak Deedar Singla <ddeedar.cse18@chitkarauniversity.edu.in>
2021-08-20 22:21:10 +0530

    flutter foundation

---
+++ flutter_foundation/android/app/src/main/kotlin/com/deepakdeedar/flutter_foundation/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.deepakdeedar.flutter_foundation
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ flutter_foundation/lib/range_selector_page.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+
+class RangeSelectorPage extends StatefulWidget {
+  RangeSelectorPage({Key? key}) : super(key: key);
+
+  @override
+  _RangeSelectorPageState createState() => _RangeSelectorPageState();
+}
+
+class _RangeSelectorPageState extends State<RangeSelectorPage> {
+  final formKey = GlobalKey<FormState>();
+  int _min = 0;
+  int _max = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Select Range'),
+      ),
+      body: Form(
+        key: formKey,
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              RangeSelectorTextFormField(
+                labelText: 'Minimum',
+                intValueSetter: (value) => _min = value,
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              RangeSelectorTextFormField(
+                labelText: 'Maximum',
+                intValueSetter: (value) => _min = value,
+              ),
+            ],
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.arrow_forward),
+        onPressed: () {
+          if (formKey.currentState?.validate() == true) {
+            formKey.currentState?.save();
+          }
+        },
+      ),
+    );
+  }
+}
+
+class RangeSelectorTextFormField extends StatelessWidget {
+  const RangeSelectorTextFormField({
+    Key? key,
+    required this.labelText,
+    required this.intValueSetter,
+  }) : super(key: key);
+
+  final String labelText;
+
+  final void Function(int value) intValueSetter;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      decoration: InputDecoration(
+        border: OutlineInputBorder(),
+        labelText: labelText,
+      ),
+      keyboardType: TextInputType.numberWithOptions(
+        decimal: false,
+        signed: true,
+      ),
+      validator: (value) {
+        if (value == null || int.tryParse(value) == null) {
+          return 'Must be an integer';
+        } else {
+          return null;
+        }
+      },
+      onSaved: (value) => intValueSetter(int.parse(value ?? '')),
+    );
+  }
+}
